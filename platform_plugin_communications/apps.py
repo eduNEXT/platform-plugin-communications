2d18b96a6ee761cd1d960c048c39ae7fca2ab262
Cristhian Garcia <crisgarta8@gmail.com>
2023-11-23 14:12:17 -0500

    chore: reformat all files

--- platform_plugin_communications/apps.py
+++ platform_plugin_communications/apps.py
@@ -10,4 +10,4 @@ class PlatformPluginCommunicationsConfig(AppConfig):
     Configuration for the platform_plugin_communications Django application.
     """
 
-    name = 'platform_plugin_communications'
+    name = "platform_plugin_communications"
