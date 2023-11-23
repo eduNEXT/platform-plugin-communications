6602ecf393cd0d2ac5638d14c9961153b478e755
Cristhian Garcia <crisgarta8@gmail.com>
2023-11-23 14:07:02 -0500

    chore: initial commit

---
+++ platform_plugin_communications/__init__.py
@@ -0,0 +1,5 @@
+"""
+An Open edX plugin that extends email capabiliplatform
+"""
+
+__version__ = '0.1.0'
---
+++ platform_plugin_communications/apps.py
@@ -0,0 +1,13 @@
+"""
+platform_plugin_communications Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class PlatformPluginCommunicationsConfig(AppConfig):
+    """
+    Configuration for the platform_plugin_communications Django application.
+    """
+
+    name = 'platform_plugin_communications'
